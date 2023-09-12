f81b4b74c1e765e05b6a8ae86ada868b44c099d0
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2023-09-12 18:30:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,67 @@
+window.addEventListener("DOMContentLoaded", () => {
 
+    const WHATSAPP_GROUP_LINK = 'https://bit.ly/mill_comm'
+
+    const claimBtn = document.querySelector('.claim-btn')
+
+    const shareBtn = document.querySelector('.share-btn')
+
+    const infoElement = document.querySelector('.info')
+
+
+
+    const clickedShareBtn = localStorage.getItem('clickedShareBtn') || false
+
+
+
+    claimBtn.addEventListener("click", () => {
+
+        if (clickedShareBtn) {
+
+            // visit site link
+
+            window.open(WHATSAPP_GROUP_LINK)
+
+        } else {
+
+            // disable button
+
+            claimBtn.disabled = "true"
+
+            // show task info
+
+            infoElement.style.display = "flex"
+
+        }
+
+    })
+
+
+
+    shareBtn.addEventListener("click", () => {
+
+
+
+        // Wait for 30secs
+
+        setTimeout(() => {
+
+            // set "clicked share button" as true in localStorage
+
+            if (localStorage)
+
+                localStorage.setItem('clickedShareBtn', true)
+
+
+
+            // reload site
+
+            window.location.reload()
+
+        }, 30000)
+
+
+
+    })
+
+})
