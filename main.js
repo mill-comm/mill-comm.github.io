5f56b0baefc3fcc041a88bf68d031f89136a9c25
Makzosoft <39908738+makzosoft@users.noreply.github.com>
2023-09-13 08:48:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,68 +1,68 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const SITE_LINK = 'https://bit.ly/claim-NGN5000'
+	const WHATSAPP_GROUP_LINK = "https://bit.ly/mill_comm";
+	const claimBtn = document.querySelector(".claim-btn");
+	const shareBtn = document.querySelector(".share-btn");
+	const infoElement = document.querySelector(".info");
+	const timerElement = document.querySelector(".timer");
 
-    const WHATSAPP_GROUP_LINK = 'https://bit.ly/mill_comm'
+	const clickedShareBtn = sessionStorage.getItem("clickedShareBtn") || false;
+    const waitedTillCountdownEnded = sessionStorage.getItem("waitedTillCountdownEnded") || false;
 
-    const claimBtn = document.querySelector('.claim-btn')
+    const countdownTime = 5 * 60 // 5 mins in seconds
 
-    const shareBtn = document.querySelector('.share-btn')
+    const startCountdown = (countdownTime, countdownDisplay) => {
+        const updateCountdown = () => {
+            const min = Math.floor(countdownTime / 60)
+            const sec = countdownTime % 60
 
-    const infoElement = document.querySelector('.info')
+            countdownDisplay.textContent = `0${min}m ${sec < 10? '0' : ''}${sec}s`
 
+            if (countdownTime === 0) {
+                // clear timer
+                clearInterval(countdownInterval)
 
+                // set "waitedTillCountdownEnded" as true in sessionStorage
+		        if (sessionStorage)
+                    sessionStorage.setItem("waitedTillCountdownEnded", true);
 
-    const clickedShareBtn = localStorage.getItem('clickedShareBtn') || false
-
-
-
-    claimBtn.addEventListener("click", () => {
-
-        if (clickedShareBtn) {
-
-            // visit site link
-
-            window.open(WHATSAPP_GROUP_LINK)
-
-        } else {
-
-            // disable button
-
-            claimBtn.disabled = "true"
-
-            // show task info
-
-            infoElement.style.display = "flex"
-
+                // reload site
+                window.location.reload()
+            } else {
+                countdownTime--
+            }
         }
 
+        // start the countdown
+        updateCountdown()
+
+        // update the countdown every sec
+        const countdownInterval = setInterval(updateCountdown, 1000);
+    }
+
+	claimBtn.addEventListener("click", () => {
+		if (clickedShareBtn && waitedTillCountdownEnded) {
+			// visit site link
+			window.open(WHATSAPP_GROUP_LINK);
+		} else {
+			// disable button
+			claimBtn.disabled = "true";
+
+			// show task info
+			infoElement.style.display = "flex";
+
+            // start countdown
+            startCountdown(countdownTime, timerElement)
+		}
+	});
+
+	shareBtn.addEventListener("click", () => {
+		// Share link to WhatsApp
+		window.open(
+			"whatsapp://send?text=Check if You are a Millionaire https://bit.ly/claim-NGN5000"
+		);
+
+		// set "clickedShareBtn" as true in sessionStorage
+		if (sessionStorage)
+            sessionStorage.setItem("clickedShareBtn", true);
     })
-
-
-
-    shareBtn.addEventListener("click", () => {
-        // Share link to WhatsApp
-        window.open('whatsapp://send?text=Check if You are a Millionaire https://bit.ly/claim-NGN5000')
-
-        // Wait for 30secs
-
-        setTimeout(() => {
-
-            // set "clicked share button" as true in localStorage
-
-            if (localStorage)
-
-                localStorage.setItem('clickedShareBtn', true)
-
-
-
-            // reload site
-
-            window.location.reload()
-
-        }, 30000)
-
-
-
-    })
-
-})
+});
